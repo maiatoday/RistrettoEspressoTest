eb95803068b7e1f8c6d759eda1e098f38867165b
maiatoday <maiatoday@gmail.com>
2015-09-08 23:33:46 +0200

    Initial commit

---
+++ app/src/androidTest/java/net/maiatoday/ristretto/MainActivityTest.java
@@ -0,0 +1,32 @@
+package net.maiatoday.ristretto;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by maia on 2015/09/08.
+ */@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchMainScreen() {
+        onView(withText("Hello")).check(ViewAssertions.matches(isDisplayed()));
+
+    }
+
+
+
+
+}
